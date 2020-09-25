0f33f1cbad1670335e1f2483dba4c728077b084a
root <towerbamslayer@gmail.com>
2020-09-25 05:14:10 +0000

    eight commit

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,5 +5,5 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
 scp -i /opt/prod /tmp/.auth service_user@172.31.32.163:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish service_user@172.31.32.163:/tmp/publish
+#scp -i /opt/prod ./jenkins/deploy/publish service_user@172.31.32.163:/tmp/publish
 ssh -i /opt/prod service_user@172.31.32.163 "/tmp/publish"
